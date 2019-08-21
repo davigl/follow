32491bb811330f72340986cb42b4a12bae46509e
Davi Guimarães <davi.guimaraesleite@gmail.com>
2019-08-21 16:39:31 -0300

    getting done.

--- github.rb
+++ github.rb
@@ -2,6 +2,9 @@ require 'httparty'
 require 'base64'
 
 module Github
+
+	# Following a specific github user, using basic auth.
+
 	def follow_user(username, password, fuser)
 		credentials = Base64.encode64("#{username}:#{password}")
 		base_uri = URI("https://api.github.com/user/following/#{fuser}")
@@ -10,15 +13,28 @@ module Github
 			'Authorization' => ('Basic ' + credentials),
 			'User-Agent' => 'Patassaura'
 		})
+
+		puts "followed #{fuser}"
 	end
 
-	def follow_all_users(fuser)
-		base_uri = URI("https://api.github.com/users/#{fuser}/followers")
+	# Getting all the followers from a github user.
+
+	def get_followers(fuser, current_page)
+		base_uri = URI("https://api.github.com/users/#{fuser}/followers?page=#{current_page}")
 
 		response = HTTParty.get(base_uri.to_s, headers: {
 			'User-Agent' => 'Patassaura'	
-		}).body
+		}).parsed_response
 
-		p response
+		return response
+	end
+
+	# Method that iterates through json and starts to follow everyone.
+
+	def follow_all_users(username, password, json)
+		json.each do |user|
+			login_user = user['login']
+			follow_user(username, password, login_user)
+		end
 	end
 end
--- user.rb
+++ user.rb
@@ -3,19 +3,36 @@ require_relative 'github'
 include Github
 
 class User
+	# Constructor that receives git username and password.
+
 	def initialize(username, password)
 		@username, @password = username, password
 	end
+
+	# Method to follow one github user.
 
 	def follow(user)
 		follow_user(@username, @password, user)
 	end
 
+	# Method that loops through user following page and follow their followers until the last page.
+
 	def follow_all(user)
-		follow_all_users(user)
+		current_page = 1
+
+		loop do
+			followers = get_followers(user, current_page)
+
+			current_page += 1
+
+			followers.any? ? follow_all_users(@username, @password, followers) : break
+		end
 	end
 end
 
-user = User.new("credentials", "credentials")
+# Receiving params, username, password, followers_user
 
-user.follow_all("test")
+followers_user = ARGV[2].to_s
+user = User.new(ARGV[0].to_s, ARGV[1].to_s)
+
+user.follow_all(followers_user)
